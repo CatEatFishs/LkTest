c72c816b80180f666e98c754b1b2a31bcd062c75
lm <1002464056@qq.com>
2020-12-10 23:25:11 +0800

    intent

--- app/src/main/java/com/lk/MainActivity.kt
+++ app/src/main/java/com/lk/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.lk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val a=0
+    var intent:Intent?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        intent = Intent(this,SubjectInfo::class.java)
     }
 
     fun jumpInfoCLick(view: View) {
         when(view.id){
-            nineteen.id-> print("19")
+            nineteen.id-> {
+
+                intent.putExtra("num",19)
+                startActivity(intent)
+            }
             twenty.id -> print("20")
         }
     }
